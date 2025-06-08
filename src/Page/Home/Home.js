5e868c593c63815d4c698ce3de5cf3894dd86a56
tarek421 <tarekmahmud740@gmail.com>
2025-06-08 11:55:01 +0600

    initial commit

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -3,13 +3,13 @@ import Headers from './Headers/Headers';
 import WhyChooseUs from './WhyChooseUs/WhyChooseUs';
 import Brands from './Brands/Brands';
 import Certificate from './Certificate/Certificate';
-import SK_About from './About_of_SK/SK_About';
+import Sk_About from './About_of_SK/SK_About';
 
 function Home() {
   return (
     <div>
         <Headers />
-        <SK_About />
+        <Sk_About />
         <WhyChooseUs />
         <Brands />
         <Certificate />

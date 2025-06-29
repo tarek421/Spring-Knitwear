d1a1d4cc915524a650990775671018c504a6cc7e
tarek421 <tarekmahmud740@gmail.com>
2025-06-30 00:45:24 +0600

    initial commit

--- src/Page/Shared/Navbar.js
+++ src/Page/Shared/Navbar.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import './Navbar.css';
 import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 import logo from '../../image/logo.svg';
-
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -13,15 +12,14 @@ const Navbar = () => {
     let timeoutId;
     const handleScroll = () => {
       const scrollY = window.scrollY;
-
-      if (scrollY > 200){
+      if (scrollY > 200) {
         setVisible(false);
         clearTimeout(timeoutId);
         timeoutId = setTimeout(() => {
           setSticky(true);
           setVisible(true);
         }, 50);
-      } else if (scrollY <= 200) {
+      } else {
         setSticky(false);
         setVisible(true);
       }
@@ -32,12 +30,16 @@ const Navbar = () => {
       clearTimeout(timeoutId);
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [sticky]);
+  }, []);
+
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? 'hidden' : 'auto';
+  }, [menuOpen]);
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-    <nav className={`navbar ${sticky ? 'sticky' : ''} ${visible ? 'show' : 'hide'}`}>
+    <nav className={`navbar ${sticky ? 'sticky' : ''} ${visible ? 'show' : 'hide'}`} role="navigation">
       <div className="logo">
         <NavLink to="/" className="d-flex align-items-center">
           <img src={logo} alt="Spring Knitwear Logo" />
@@ -45,17 +47,21 @@ const Navbar = () => {
         </NavLink>
       </div>
 
-      <div className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+      <button
+        className={`menu-toggle ${menuOpen ? 'open' : ''}`}
+        onClick={toggleMenu}
+        aria-label="Toggle navigation menu"
+      >
         <div className="bar"></div>
         <div className="bar"></div>
         <div className="bar"></div>
-      </div>
+      </button>
 
       <ul className={`menu ${menuOpen ? 'active' : ''}`}>
-        <li><NavLink to='/home' onClick={() => setMenuOpen(false)}>Home</NavLink></li>
-        <li><NavLink to='/about' onClick={() => setMenuOpen(false)}>About</NavLink></li>
-        <li><NavLink to='/products' onClick={() => setMenuOpen(false)}>Products</NavLink></li>
-        <li><NavLink to='/contacts' onClick={() => setMenuOpen(false)}>Contact</NavLink></li>
+        <li><NavLink to="/" onClick={() => setMenuOpen(false)}>Home</NavLink></li>
+        <li><NavLink to="/about" onClick={() => setMenuOpen(false)}>About</NavLink></li>
+        <li><NavLink to="/products" onClick={() => setMenuOpen(false)}>Products</NavLink></li>
+        <li><NavLink to="/contacts" onClick={() => setMenuOpen(false)}>Contact</NavLink></li>
       </ul>
     </nav>
   );
